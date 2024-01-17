8c4624e3c38a32e68cefc82e887513a9dd096b8a
Jaymin Bhavsar <jayminbhavsar07@gmail.com>
2024-01-17 11:11:38 +0000

    added index and script

---
+++ script.js
@@ -0,0 +1,36 @@
+var products = [
+    {
+        name: "Nike",
+        description: "Air Force 1 Jester XX Black Sonic Yellow ...",
+        price: "$96"
+    },
+    {
+        name: "Converse",
+        description: "Run Star Hike Three Color Unisex",
+        price: "$85.5"
+    },
+    {
+        name: "Nike",
+        description: "Air Jordan 1 Retro High Obsidian UNC",
+        price: "$196"
+    },
+    {
+        name: "Nike",
+        description: "Air Force 1 Shadow Beige Pale Ivory",
+        price: "$115"
+    },
+    {
+        name: "New Balance",
+        description: "Air Force 1 Jester XX Black Sonic Yellow ...",
+        price: "$96"
+    },
+    {
+        name: "Asics",
+        description: "Air Force 1 Jester XX Black Sonic Yellow ...",
+        price: "$96"
+    }
+];
+
+const list = document.getElementById("product-list");
+
+document.body.innerHTML = products.length
